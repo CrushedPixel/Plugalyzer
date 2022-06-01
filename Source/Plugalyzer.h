23d47b649205d540c166371665c7dc07aa652bd5
Marius Metzger <marius@crushedpixel.net>
2022-06-02 01:25:15 +0300

    Add listParameters command to list the plugin's parameters

--- Source/Plugalyzer.h
+++ Source/Plugalyzer.h
@@ -3,6 +3,7 @@
 #include <juce_audio_formats/juce_audio_formats.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 
-void plugalyze(const juce::String& pluginPath, const std::vector<juce::File>& inputFiles,
-               const juce::File& outputFile, int blockSize,
-               std::optional<int> numOutputChannelsOpt);
+void process(const juce::String& pluginPath, const std::vector<juce::File>& inputFiles,
+             const juce::File& outputFile, int blockSize, std::optional<int> numOutputChannelsOpt);
+
+void listParameters(const juce::String& pluginPath, double initialSampleRate, int initialBlockSize);
--- Source/main.cpp
+++ Source/main.cpp
@@ -2,22 +2,21 @@
 #include <juce_audio_formats/juce_audio_formats.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 
-void runCommandLine(const juce::String& commandLineParameters) {
-    juce::ArgumentList args("", commandLineParameters);
+std::vector<juce::File> getInputFileArguments(const juce::ArgumentList& _args) {
+    // create a copy to keep the original argument list unmodified
+    // when consuming the input options
+    juce::ArgumentList args = _args;
 
-    // parse plugin option
-    args.failIfOptionIsMissing("--plugin");
-    auto pluginPath = args.getValueForOption("--plugin");
-
-    // parse input file option(s).
-    // each input file will be sent to the plugin in a separate bus.
     std::vector<juce::File> inputFiles;
     args.failIfOptionIsMissing("--input");
     while (args.containsOption("--input")) {
         inputFiles.push_back(args.getExistingFileForOptionAndRemove("--input"));
     }
 
-    // parse output file option
+    return inputFiles;
+}
+
+juce::File getOutputFileArgument(const juce::ArgumentList& args) {
     args.failIfOptionIsMissing("--output");
     auto outputFile = args.getFileForOption("--output");
     if (outputFile.existsAsFile() && !args.containsOption("--overwrite")) {
@@ -25,6 +24,15 @@ void runCommandLine(const juce::String& commandLineParameters) {
             "Output file already exists! Use --overwrite to overwrite the file.");
     }
 
+    return outputFile;
+}
+
+juce::String getPluginArgument(const juce::ArgumentList& args) {
+    args.failIfOptionIsMissing("--plugin");
+    return args.getValueForOption("--plugin");
+}
+
+int getBlockSizeArgument(const juce::ArgumentList& args) {
     // parse optional block size option
     int blockSize = 1024;
     if (args.containsOption("--blockSize")) {
@@ -34,7 +42,10 @@ void runCommandLine(const juce::String& commandLineParameters) {
         }
     }
 
-    // parse optional output channel count option
+    return blockSize;
+}
+
+std::optional<int> getOutputChannelCountArgument(const juce::ArgumentList& args) {
     std::optional<int> numOutputChannelsOpt;
     if (args.containsOption("--outChannels")) {
         numOutputChannelsOpt = args.getValueForOption("--outChannels").getIntValue();
@@ -43,7 +54,62 @@ void runCommandLine(const juce::String& commandLineParameters) {
         }
     }
 
-    plugalyze(pluginPath, inputFiles, outputFile, blockSize, numOutputChannelsOpt);
+    return numOutputChannelsOpt;
+}
+
+double getSampleRateArgument(const juce::ArgumentList& args) {
+    // parse optional sample rate option
+    double sampleRate = 44100;
+    if (args.containsOption("--sampleRate")) {
+        sampleRate = args.getValueForOption("--sampleRate").getDoubleValue();
+        if (sampleRate <= 0) {
+            juce::ConsoleApplication::fail("sampleRate must be a positive number");
+        }
+    }
+
+    return sampleRate;
+}
+
+void runProcessCommand(const juce::ArgumentList& args) {
+    // parse plugin option
+    auto pluginPath = getPluginArgument(args);
+
+    // parse input file option(s).
+    // each input file will be sent to the plugin in a separate bus.
+    auto inputFiles = getInputFileArguments(args);
+
+    auto outputFile = getOutputFileArgument(args);
+    auto blockSize = getBlockSizeArgument(args);
+
+    // parse optional output channel count option
+    auto numOutputChannelsOpt = getOutputChannelCountArgument(args);
+
+    process(pluginPath, inputFiles, outputFile, blockSize, numOutputChannelsOpt);
+}
+
+void runListParametersCommand(const juce::ArgumentList& args) {
+    auto pluginPath = getPluginArgument(args);
+    auto sampleRate = getSampleRateArgument(args);
+    auto blockSize = getBlockSizeArgument(args);
+
+    listParameters(pluginPath, sampleRate, blockSize);
+}
+
+void runCommandLine(const juce::String& commandLineParameters) {
+    juce::ArgumentList args("", commandLineParameters);
+
+    if (args.arguments.isEmpty()) {
+        juce::ConsoleApplication::fail(
+            "Must have at least one argument to indicate the desired action");
+    }
+    auto command = args.arguments.removeAndReturn(0).text;
+    if (command == "process") {
+        runProcessCommand(args);
+    } else if (command == "listParameters") {
+        runListParametersCommand(args);
+    } else {
+        juce::ConsoleApplication::fail("Invalid command: " + command);
+    }
 }
 
 class PlugalyzerApplication : public juce::JUCEApplicationBase {
